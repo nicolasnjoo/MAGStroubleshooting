e98dae5b1d5aae0144abbef2fd16a6af2c97c75f
nicolasnjoo <91150451+nicolasnjoo@users.noreply.github.com>
2021-09-21 16:50:48 -0400

    Update main.js

--- network/js/main.js
+++ network/js/main.js
@@ -372,7 +372,7 @@ function Search(a) {
         if (2 >= a.length) this.results.html("<i>You must search for a name with a minimum of 3 letters.</i>");
         else {
             sigInst.iterNodes(function (a) {
-                g.test(a.label.toLowerCase()) && c.push({
+                g.test(a.diagnosis.toLowerCase()) && c.push({
                     id: a.id,
                     name: a.label
                 })
